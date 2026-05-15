cd3df64a9ce537c0d167ae68437e6a3aac0e06c4
Amir Mohammadi <amirmhmdi95@gmail.com>
2026-05-15 13:03:18 +0100

    header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header()
+{
+    return (
+        <header>
+            <nav className="nav">
+                <img src="src/assets/globe.png" alt="globe icon" />
+                <h1 className="nav--logo_text">my travel journal.</h1>
+            </nav>
+        </header>
+    )
+}
