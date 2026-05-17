9332bd4319ebc4d958cf77a4857979ba023ee896
Amir Mohammadi <amirmhmdi95@gmail.com>
2026-05-17 20:09:22 +0100

    read list of data and show components

--- src/App.tsx
+++ src/App.tsx
@@ -2,37 +2,27 @@
 import './App.css'
 import Header from './components/Header'
 import Entry from './components/Entry'
+import data from './data'
+
 
 
 function App()
 {
-
+  let entries = data.map((entry) => (
+    <Entry
+      id={entry.id}
+      img={entry.img}
+      title={entry.title}
+      country={entry.country}
+      googleMapsLink={entry.googleMapsLink}
+      dates={entry.dates}
+      text={entry.text}
+    />
+  ));
   return (
     <>
       <Header />
-      <Entry
-        img={{ src: 'src/assets/japan.png', alt: 'Japan Image' }}
-        countryName="Japan"
-        cityName="Tokyo"
-        date="2023-10-15"
-        description="Amazing experience in the land of the rising sun!"
-      />
-
-      <Entry
-        img={{ src: 'src/assets/sydney.png', alt: 'Sydney Image' }}
-        countryName="Australia"
-        cityName="Sydney"
-        date="2023-10-16"
-        description="Incredible city by the harbor!"
-      />
-
-      <Entry
-        img={{ src: 'src/assets/geirangerfjord.png', alt: 'Geirangerfjord Image' }}
-        countryName="Norway"
-        cityName="Geiranger"
-        date="2023-10-17"
-        description="Breathtaking fjord with stunning waterfalls!"
-      />
+      {entries}
 
     </>
   )
--- src/components/Entry.tsx
+++ src/components/Entry.tsx
@@ -1,28 +1,31 @@
 
 interface EntryProps
 {
+    id: number
     img: { src: string, alt: string }
-    countryName: string
-    cityName: string
-    date: string
-    description: string
+    title: string
+    country: string
+    googleMapsLink: string
+    dates: string
+    text: string
 }
 
 
-export default function Entry({ img, countryName, cityName, date, description }: EntryProps)
+export default function Entry({ id, img, title, country, googleMapsLink, dates, text }: EntryProps)
 {
+    console.log(id);
     return (
         <div className="entry-main-card">
             <img src={img.src} className="entry-main-image" alt={img.alt} />
             <div >
                 <div className="entry-first-line">
                     <img src="src/assets/marker.png" alt="Marker icon" />
-                    <span>{countryName}</span>
-                    <a href="google.com" target="_blank" rel="noopener noreferrer">View on Google Map</a>
+                    <span>{country}</span>
+                    <a href={googleMapsLink} target="_blank" rel="noopener noreferrer">View on Google Map</a>
                 </div>
-                <h2>{cityName}</h2>
-                <h3>{date}</h3>
-                <p className="entry-discription">{description}</p>
+                <h2>{title}</h2>
+                <h3>{dates}</h3>
+                <p className="entry-discription">{text}</p>
             </div>
         </div>
     )
